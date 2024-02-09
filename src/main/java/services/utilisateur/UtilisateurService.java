fb8f62068a2d0338c48699fa7bd0ce97978de37b
welhazitakwa <welhazitakwa@gmail.com>
2024-02-09 19:06:38 +0100

    correction noms fichiers

--- src/main/java/services/utilisateur/UtilisateurService.java
+++ src/main/java/services/utilisateur/UtilisateurService.java
@@ -1,12 +1,2 @@
-package services.utilisateur;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface UtilisateurService <T> {
-
-     void ajouter (T t) throws SQLException ;
-     void modifier (T t) throws SQLException ;
-     void supprimer (int id) throws SQLException ;
-    List <T> listAll () throws SQLException ;
+package services.utilisateur;public class UtilisateurService {
 }
