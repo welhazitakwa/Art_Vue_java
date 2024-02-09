5cabd8135928229945237a1fa2248593368a69de
oumeyma <oumeyma.benkram@esprit.tn>
2024-02-09 22:09:18 +0100

    Oumeyma

--- src/main/java/models/Commande.java
+++ src/main/java/models/Commande.java
@@ -1,4 +1,58 @@
 package models;
+import java.util.Date;
 
 public class Commande {
+    public int id;
+    public float montant;
+    public Date date;
+    public String etat;
+
+    public Commande(int id, float montant, Date date, String etat) {
+        this.id = id;
+        this.montant = montant;
+        this.date = date;
+        this.etat = etat;
+    }
+
+    public Commande(float montant, Date date, String etat) {
+        this.montant = montant;
+        this.date = date;
+        this.etat = etat;
+    }
+
+    public Commande() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getMontant() {
+        return montant;
+    }
+
+    public void setMontant(float montant) {
+        this.montant = montant;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
 }
--- src/main/java/services/Commande/ICommande.java
+++ src/main/java/services/Commande/ICommande.java
@@ -1,4 +1,12 @@
 package services.Commande;
 
-public interface ICommande {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ICommande<C> {
+    void AjouterCommande (C c) throws SQLException;
+    void ModifierCommande (C c) throws SQLException;
+    void SupprimerCommande (int id ) throws SQLException;
+    List<C> AfficherCommande ( ) throws SQLException;
+
 }
