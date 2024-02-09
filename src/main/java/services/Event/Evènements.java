85b3f7a1f710f85154b887a839db931065f17033
arijabidi <arijabidi577@gmail.com>
2024-02-09 17:47:48 +0100

    Event

---
+++ src/main/java/services/Event/Evènements.java
@@ -0,0 +1,11 @@
+package services.Event;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Evènements<T>{
+    void ajouterevent(T t) throws SQLException;
+    void modifier(T t) throws SQLException;
+    void supprimer(int id) throws SQLException;
+    List<T> recuperer() throws SQLException;
+}
