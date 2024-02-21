f84ba85f17e9b910e94c4ca572e2ff889c0a9db4
welhazitakwa <welhazitakwa@gmail.com>
2024-02-21 14:40:46 +0100

    register ans se_connecter button

--- src/main/java/Controles/Register.java
+++ src/main/java/Controles/Register.java
@@ -4,13 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import services.utilisateur.UtilisateurService;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Register {
     @FXML
@@ -57,7 +56,33 @@ public class Register {
 
     @FXML
     void sinscrireButton(ActionEvent event) {
-
+//        UtilisateurService user1 = new UtilisateurService();
+//        //int validLogin = 5;
+//        try {
+//            validLogin = user1.validateLogin(loginTextField.getText(), mdpTextField.getText());
+//            //  System.out.println(loginTextField.getText());
+//            //  System.out.println(mdpTextField.getText());
+//        } catch (SQLException e) {
+//            Alert alert2 = new Alert(Alert.AlertType.ERROR);
+//            alert2.setContentText(e.getMessage());
+//            alert2.show();
+//        }
+//        if (validLogin == 0) {
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setContentText("c'est un admin");
+//            alert.show();
+//        } else if (validLogin == 1) {
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setContentText("c'est un artiste");
+//            alert.show();
+//        }else if (validLogin == 2) {
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setContentText("c'est un client");
+//            alert.show();
+//
+//        } else {
+//            labelError.setText("Merci de vérifier vos données !");
+//        }
     }
 
 }
