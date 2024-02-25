bc7c5d4e81d66ab19087ea7ed569f10bc68bd153
welhazitakwa <welhazitakwa@gmail.com>
2024-02-25 21:17:19 +0100

    confirmation par alert

--- src/main/java/Controles/ModifierProfil.java
+++ src/main/java/Controles/ModifierProfil.java
@@ -3,10 +3,7 @@ package Controles;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import models.Utilisateur;
 import services.utilisateur.UtilisateurService;
 
@@ -55,6 +52,11 @@ public class ModifierProfil implements Initializable {
                     id, nomEdit.getText(), prenomEdit.getText(),mailEdit.getText(),
                     Integer.parseInt(numTelEdit.getText()), loginEdit.getText(), Integer.parseInt(cinEdit.getText()),"cccccc ", comboGenre.getValue(),
                     Date.valueOf(datePicker.getValue()), adresseEdit.getText()));
+            Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+            dialog.setTitle("Alerte de Confirmation");
+            dialog.setHeaderText(" La modification est effectué avec succès ");
+            dialog.setContentText("Votre Compte est à jour");
+            dialog.showAndWait();
         } catch (SQLException s){
             System.out.println(s.getMessage());
         }
@@ -77,7 +79,7 @@ public class ModifierProfil implements Initializable {
 
             nomEdit.setText(user1.getUtilisateurById(id).getNom());
             prenomEdit.setText( user1.getUtilisateurById(id).getPrenom() );
-            adresseEdit.setText( user1.getUtilisateurById(id).getEmail());
+            adresseEdit.setText( user1.getUtilisateurById(id).getAdresse());
             comboGenre.setValue(user1.getUtilisateurById(id).getGenre());
             cinEdit.setText(String.valueOf(user1.getUtilisateurById(id).getCin()));
             // Supposons que vous ayez un DatePicker appelé datePicker et un Label appelé dateLabel
@@ -85,6 +87,7 @@ public class ModifierProfil implements Initializable {
             numTelEdit.setText(String.valueOf(user1.getUtilisateurById(id).getNumTel()));
             mailEdit.setText(user1.getUtilisateurById(id).getEmail());
             loginEdit.setText(user1.getUtilisateurById(id).getLogin());
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
