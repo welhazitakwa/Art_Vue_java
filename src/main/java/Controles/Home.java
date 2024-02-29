7edefab825a17b235bcdc1306be54a6cbbf3bafb
khalil27 <khalil.taallah@esprit.tn>
2024-02-29 10:51:13 +0100

    erreur

--- src/main/java/Controles/Home.java
+++ src/main/java/Controles/Home.java
@@ -17,8 +17,10 @@ public class Home extends Application {
     @Override
     public void start(Stage primaryStage) {
           //FXMLLoader loader=new FXMLLoader(getClass().getResource("/AjouterCategorie.fxml"));
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/AdminDashboard.fxml"));
+       // FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/AdminDashboard.fxml"));
        // FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/fxmlClient/Acceuil.fxml"));
+
 
         try {
             Parent root = loader.load();
