f33c22f22cde6d5afada962930fab4ab4225a1ab
welhazitakwa <welhazitakwa@gmail.com>
2024-03-02 19:46:47 +0100

    initialisation forget password

--- src/main/java/Controles/ForgetPassword.java
+++ src/main/java/Controles/ForgetPassword.java
@@ -1,4 +1,34 @@
 package Controles;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class ForgetPassword {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField EmailField;
+
+    @FXML
+    private Label ErrorLabel;
+
+    @FXML
+    void Valider(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+    }
+
 }
