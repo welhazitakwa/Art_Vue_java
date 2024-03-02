9813751ddfc5d05e88ca539ede69b3c2c0bbe2ba
welhazitakwa <welhazitakwa@gmail.com>
2024-03-02 15:22:16 +0100

    recuperation des listes des utilisateurs dans le pdf

--- src/main/java/Controles/PDFGenerator.java
+++ src/main/java/Controles/PDFGenerator.java
@@ -7,41 +7,51 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.IOException;
 import java.util.List;
 
-public class PDFGenerator {
+    public class PDFGenerator {
 
-    public static void generatePDF(List<String> userList, String outputPath) {
-        try {
-            PDDocument document = new PDDocument();
-            PDPage page = new PDPage();
-            document.addPage(page);
+        public static void generatePDF(List<String> userList1, List<String> userList2, String outputPath) {
+            try {
+                PDDocument document = new PDDocument();
+                PDPage page = new PDPage();
+                document.addPage(page);
 
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+                PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            contentStream.beginText();
-            contentStream.newLineAtOffset(50, 700);
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.beginText();
+                contentStream.newLineAtOffset(50, 700);
 
-            contentStream.showText("Liste des utilisateurs :");
-            contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Liste des utilisateurs de la première liste :");
+                contentStream.newLineAtOffset(0, -20);
 
-            for (String user : userList) {
-                contentStream.showText(user);
-                contentStream.newLineAtOffset(0, -15);
+                for (String user : userList1) {
+                    contentStream.showText(user);
+                    contentStream.newLineAtOffset(0, -15);
+                }
+
+                contentStream.newLineAtOffset(0, -20);
+
+                contentStream.showText("Liste des utilisateurs de la deuxième liste :");
+                contentStream.newLineAtOffset(0, -20);
+
+                for (String user : userList2) {
+                    contentStream.showText(user);
+                    contentStream.newLineAtOffset(0, -15);
+                }
+
+                contentStream.endText();
+                contentStream.close();
+
+                document.save(outputPath);
+                document.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            contentStream.endText();
-            contentStream.close();
-
-            document.save(outputPath);
-            document.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
 
-    public static void main(String[] args) {
-        // Exemple d'utilisation :
-        List<String> userList = List.of("Utilisateur 1", "Utilisateur 2", "Utilisateur 3");
-        generatePDF(userList, "C:\\Users\\Fatma Ouelhazi\\Downloads\\output.pdf");
-    }
+//    public static void main(String[] args) {
+//        // Exemple d'utilisation :
+//        List<String> userList = List.of("Utilisateur 1", "Utilisateur 2", "Utilisateur 3");
+//        generatePDF(userList, "C:\\Users\\Fatma Ouelhazi\\Downloads\\output.pdf");
+//    }
 }
